48b47451e7293484f62caf1893dd5341c7beb249
EC2 Default User <ec2-user@ip-172-31-42-157.us-west-2.compute.internal>
2022-07-30 05:18:04 +0000

    Added my project files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="akobulife/udacitymicro:v1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitymicro --image=akobulife/udacitymicro:v1.0 --port=80 --labels app=udacitymicro
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacitymicro 8000:80
 
